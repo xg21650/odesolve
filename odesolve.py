55777c24814285c544299d0fe2ab4d0e6529d6d8
xg21650 <xg21650@bristol.ac.uk>
2022-07-23 14:41:54 +0100

    first commit

---
+++ odesolve.py
@@ -0,0 +1,26 @@
+# odesolve.py
+#
+# Author: <xg21650>
+# Date: <24 7 22>
+# Description <This is a program that will solve ordinary differential equations (ODEs)>
+
+
+def euler(f, x, t, h):
+
+    pass
+
+
+def rk4(f, x, t, h):
+
+    pass
+
+
+def solveto(f, x1, t1, t2, hmax, method=euler):
+
+    pass
+
+
+def odesolve(f, X0, t, hmax, medthod=euler):
+
+    pass
+
