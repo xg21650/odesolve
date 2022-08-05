82372af4a29ec25477f7338944ff7b112beab6d2
xg21650 <xg21650@bristol.ac.uk>
2022-08-05 11:08:42 +0100

    final

--- odesolve.py
+++ odesolve.py
@@ -12,11 +12,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-f = 0
-
 
 def euler(f, x, t, h):
-    return x + f(x, t) * h
+    return x + f(x, t) * h  # returns a step of the euler method
     pass
 
 
@@ -25,74 +23,45 @@ def rk4(f, x, t, h):
     k2 = f(x + k1 * h / 2, t + h / 2)
     k3 = f(x + k2 * h / 2, t + h / 2)
     k4 = f(x + k3 * h, t + h)
-    return x + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)
+    return x + (h / 6) * (k1 + 2 * k2 + 2 * k3 + k4)  # returns a step of the rk4 method
     pass
 
 
 def solveto(f, x0, t0, t1, hmax, method=euler):
     h = hmax
-    for i in np.arange(t0, t1, hmax):
+    for i in np.arange(t0, t1, hmax):  # repeats for each equal interval between t0 and t1
         if method == euler:
-            if i > t1 - hmax:
-                h = t1 - i
-            x0 = euler(f, x0, i, h)
+            if i > t1 - hmax:  # checks if the step size is not divisible by t1
+                h = t1 - i  # adjusts h so that the final step size will land on t1
+            x0 = euler(f, x0, i, h)  # performs euler method
             pass
-
         else:
             if i > t1 - hmax:
                 h = t1 - i
-            x0 = rk4(f, x0, i, h)
+            x0 = rk4(f, x0, i, h)  # performs rk4 method
             pass
     return x0
     pass
 
 
 def odesolve(f, x0, tvals, hmax, method=euler):
-    X0 = np.array([x0])
-
-    for i in range(len(tvals) - 1):
+    X0 = np.array([x0])  # creates an array with starting x0
+    for i in range(len(tvals) - 1):  # repeats for each element in tvals
         if method == euler:
             Xt = solveto(f, x0[0], tvals[0], tvals[i + 1], hmax)
-            X0 = np.append(X0, [Xt])
+            X0 = np.append(X0, [Xt])  # appends new value Xt to the array
         else:
             Xt = solveto(f, x0[0], tvals[0], tvals[i + 1], hmax, rk4)
             X0 = np.append(X0, [Xt])
-    return X0
-
     plt.plot(tvals, X0.T)
     plt.savefig('shm.pdf')
     plt.show()
     pass
+    return X0  # returns the completed array
 
 
-def error(f, x0, t0, t1):
-    hmax = (t1 - t0) / 100
-    x1 = 0
-    y1 = 0
-    X0 = numpy.array([x1])
-    Y0 = numpy.array([y1])
-    for i in range(100):
-        hmax = (i * hmax) + t0
-        x1 = euler(f, x0, t0, hmax)
-        y1 = rk4(f, x0, t0, hmax)
-        X0 = np.append(x1)
-        Y0 = np.append(y1)
 
-    e = 2.718281828459045
 
-    errorEuler = e - solveto(f, X0, t0, t1, hmax)
-    errorRK4 = e - solveto(f, X0, t0, t1, hmax, rk4)
-    print(errorEuler)
-    print(errorRK4)
-    if errorEuler < 0:
-        errorEuler = -errorEuler
-        pass
-    elif errorRK4 < 0:
-        errorRK4 = -errorRK4
-        pass
-    plt.plot(errorEuler, color='blue')
-    plt.plot(errorRK4, color='orange')
-    pass
 
 
 
